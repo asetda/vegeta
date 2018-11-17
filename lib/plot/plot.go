dd11c57432142ee7c109d68d27e79c4ab2fdee4b
Aset Dauletbaev <aset.da@gmail.com>
2018-11-17 12:06:15 -0500

    updated deps

--- lib/plot/plot.go
+++ lib/plot/plot.go
@@ -13,8 +13,8 @@ import (
 	"strconv"
 	"time"
 
-	vegeta "github.com/tsenart/vegeta/lib"
-	"github.com/tsenart/vegeta/lib/lttb"
+	vegeta "github.com/asetda/vegeta/lib"
+	"github.com/asetda/vegeta/lib/lttb"
 )
 
 // An Plot represents an interactive HTML time series
